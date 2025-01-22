100c8c3f7f6f022bc81b2734960571c104f044c4
akshay <akshayc278@gmail.com>
2025-01-22 08:38:50 +0530

    add display dll

--- data_structure/src/DoublyLLDefinations.java
+++ data_structure/src/DoublyLLDefinations.java
@@ -27,5 +27,70 @@ public class DoublyLLDefinations {
     public int length() {
         return length;
     }
+
+    public void displayForwad(){
+        if(head == null){
+            return;
+        }
+        ListNode startNode = head;
+        while(startNode!=null){
+            System.out.print(startNode.data+" ");
+            startNode=startNode.next;
+        }
+
+    }
+
+    public void displayBackward(){
+        if(head == null){
+            return;
+        }
+        ListNode startNode = tail;
+        while(startNode!=null){
+            System.out.print(startNode.data+" ");
+            startNode=startNode.previous;
+        }
+
+    }
+
+    public void addAtEnd(int data){
+        ListNode node = new ListNode(data);
+        if(head == null){
+            head=node;
+            tail=node;
+            length++;
+            return;
+        }
+        tail.next=node;
+        node.previous=tail;
+        tail=node;
+        length++;
+    }
+
+    public void addAtFront(int data){
+        ListNode node =new ListNode(data);
+        if(head == null){
+            head=node;
+            tail=node;
+            length++;
+            return;
+        }
+        head.previous =node;
+        node.next=head;
+        head=node;
+        length++;
+    }
+
+    public static void main(String[] args) {
+        DoublyLLDefinations doublyLLDefinations = new DoublyLLDefinations();
+        doublyLLDefinations.addAtEnd(5);
+        doublyLLDefinations.addAtEnd(8);
+        doublyLLDefinations.addAtEnd(10);
+        doublyLLDefinations.displayForwad();
+        System.out.println();
+        doublyLLDefinations.displayBackward();
+        doublyLLDefinations.addAtFront(3);
+        System.out.println();
+        doublyLLDefinations.displayForwad();
+    }
 }
 
